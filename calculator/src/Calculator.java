e8374c89b25f801f683d93e99969338ad7848629
Dan Rosen <dan@marakana.com>
2012-10-23 11:15:48 -0700

    distinguish operators from garbage

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,9 +1,10 @@
-
 public class Calculator {
 
 	/**
 	 * Takes a postfix-notation expression and outputs its value.
-	 * @param args the postfix-notation expression
+	 * 
+	 * @param args
+	 *            the postfix-notation expression
 	 */
 	public static void main(String[] args) {
 
@@ -21,11 +22,31 @@ public class Calculator {
 
 		// iterate over the tokens
 		for (String token : tokens) {
+
+			// try-catch to distinguish numbers from non-numbers
 			try {
 				int number = Integer.parseInt(token);
-				System.out.println(number);
+				System.out.println(number + " is a number");
 			} catch (NumberFormatException e) {
-				System.out.println(token + " is not a number");
+
+				// valid operators are all one character in length
+				if (token.length() == 1) {
+					char operator = token.charAt(0);
+
+					// switch to distinguish operators from garbage
+					switch (operator) {
+					case '+':
+					case '-':
+					case '*':
+					case '/':
+						System.out.println(operator + " is an operator");
+						break;
+					default:
+						System.out.println(token + " is garbage");
+					}
+				} else {
+					System.out.println(token + " is garbage");
+				}
 			}
 		}
 	}
