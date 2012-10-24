f63c7e7b2e1011837dc84c69e45f7186274b98f0
Dan Rosen <dan@marakana.com>
2012-10-24 12:13:15 -0700

    more methods!

--- fun/src/FunTest.java
+++ fun/src/FunTest.java
@@ -7,22 +7,24 @@ public class FunTest {
 
 	@Test
 	public void test() {
-		ProjectorBag bag = new ProjectorBag();
-		bag.color = "Black";
-		bag.size = 0.3;
-		bag.material = "Nylon";
-		bag.open = true;
-		bag.projector = new Projector();
-		bag.removeProjector();
+		// projector needs a lens
+		Lens l = new Lens();
 
-		System.out.println(bag.color);
-		System.out.println(bag.size);
-		System.out.println(bag.material);
-		System.out.println(bag.open);
-		System.out.println(bag.projector);
-		
-		Projector anotherProjector = bag.projector;
-		System.out.println(anotherProjector);
+		// attach lens to projector
+		Projector p = new Projector();
+		p.attachLens(l);
+
+		// insert projector in bag
+		ProjectorBag b = new ProjectorBag();
+		b.insertProjector(p);
+
+		// undo all that hard work
+		Projector p2 = b.removeProjector();
+		Lens l2 = p2.removeLens();
+
+		// we should get back the same objects we put in!
+		assertSame(p2, p);
+		assertSame(l2, l);
 	}
 
 }
--- fun/src/Projector.java
+++ fun/src/Projector.java
@@ -1,4 +1,22 @@
 public class Projector {
 	boolean on;
 	Lens lens;
+
+	void attachLens(Lens lens) {
+		this.lens = lens;
+	}
+
+	Lens removeLens() {
+		Lens lens = this.lens;
+		this.lens = null;
+		return lens;
+	}
+
+	void switchOn() {
+		on = true;
+	}
+
+	void switchOff() {
+		on = false;
+	}
 }
