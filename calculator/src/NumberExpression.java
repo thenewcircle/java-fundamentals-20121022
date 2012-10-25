c80f491f2fce05a9d786e0e9d4c6904d1d46cb7f
Dan Rosen <dan@marakana.com>
2012-10-25 11:11:11 -0700

    added structural identity for expressions

--- calculator/src/NumberExpression.java
+++ calculator/src/NumberExpression.java
@@ -8,4 +8,26 @@ public class NumberExpression implements Expression {
 	public int getValue() {
 		return value;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + value;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NumberExpression other = (NumberExpression) obj;
+		if (value != other.value)
+			return false;
+		return true;
+	}
 }
--- calculator/src/OperationExpression.java
+++ calculator/src/OperationExpression.java
@@ -11,4 +11,41 @@ public class OperationExpression implements Expression {
 	public int getValue() {
 		return op.operate(lhs.getValue(), rhs.getValue());
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((lhs == null) ? 0 : lhs.hashCode());
+		result = prime * result + ((op == null) ? 0 : op.hashCode());
+		result = prime * result + ((rhs == null) ? 0 : rhs.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OperationExpression other = (OperationExpression) obj;
+		if (lhs == null) {
+			if (other.lhs != null)
+				return false;
+		} else if (!lhs.equals(other.lhs))
+			return false;
+		if (op == null) {
+			if (other.op != null)
+				return false;
+		} else if (!op.equals(other.op))
+			return false;
+		if (rhs == null) {
+			if (other.rhs != null)
+				return false;
+		} else if (!rhs.equals(other.rhs))
+			return false;
+		return true;
+	}
 }
